61eee44fd78f32f1affb742a8c46a02364d74d3b
robsonbittencourt <robson.luizv@gmail.com>
2019-04-06 08:49:29 -0300

    Add  structure and basic simulation

---
+++ src/gatling/simulations/sample/BasicSimulation.scala
@@ -0,0 +1,24 @@
+package sample
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://computer-database.gatling.io")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_1")
+    .get("/"))
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
+
+}
