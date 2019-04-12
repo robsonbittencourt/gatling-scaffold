c5345b2721ad331bb341e95e4c3a5a0b184f1933
robsonbittencourt <robson.luizv@gmail.com>
2019-04-12 19:57:14 -0300

    Simplify example

--- src/gatling/simulations/sample/BasicSimulation.scala
+++ src/gatling/simulations/sample/BasicSimulation.scala
@@ -6,20 +6,14 @@ import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
-  val httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io")
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+  val httpProtocol = http.baseUrl("http://computer-database.gatling.io")
 
   val scn = scenario("BasicSimulation")
     .exec(http("request_1")
     .get("/"))
 
   setUp(
-    scn.inject(constantUsersPerSec(10) during(10 seconds))
+    scn.inject(constantUsersPerSec(2) during(60 seconds))
   ).protocols(httpProtocol).assertions(global.responseTime.percentile3.gt(95))
 
 }
