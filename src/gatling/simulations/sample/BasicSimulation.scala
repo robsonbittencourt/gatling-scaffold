15f1dab1fa375d98662c60dd4814d4982faf0c0f
robsonbittencourt <robson.luizv@gmail.com>
2019-04-27 19:11:41 -0300

    Update BasicSimulation

--- src/gatling/simulations/sample/BasicSimulation.scala
+++ src/gatling/simulations/sample/BasicSimulation.scala
@@ -1,19 +1,25 @@
 package sample
 
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
+import scala.util.Random
 import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
-  val httpProtocol = http.baseUrl("http://computer-database.gatling.io")
+  val httpProtocol = http.baseUrl("https://httpstat.us/500")
+
+  def random() = Random.nextInt(5000)
 
   val scn = scenario("BasicSimulation")
-    .exec(http("request_1")
-    .get("/"))
+    .exec(http("Should be ok").get("/200"))
+    .exec(http("Gateway timeout").get("/504"))
+    .exec(http("With random response time").get(s"/200?sleep=${random()}"))
 
   setUp(
     scn.inject(constantUsersPerSec(2) during(60 seconds))
-  ).protocols(httpProtocol).assertions(global.responseTime.percentile3.gt(95))
+  ).protocols(httpProtocol)
 
 }
